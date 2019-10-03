dd384a977969f1e6aec8624d567f22b3f9b57d49
dfrancese <davefrancese@gmail.com>
2019-10-02 21:34:38 -0400

    psuedo coding auth routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,23 @@ module.exports = app => {
     const data = await getAllUsers();
     res.send(data)
   })
+
+  app.post('/auth/signUp', async (req, res, next) => {
+    // take in req.body.email & req.body.password
+    // hash and salt email & password
+    // save in 'users' table
+    // make token
+    // send token to frontend to save in local storage
+  })
+
+  app.post('/auth/login', async (req, res, next) => {
+    // take in req.body.email & req.body.password
+    // bcrypt compare
+    // if true: make token
+    // send token to frontend to save in local storage
+  })
+
+  app.post('/auth/logout', async (req, res, next) => {
+    // revoke token
+  })
 }
